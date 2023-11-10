73dcc3bfd1338f8ef24200da92fe123e46285e16
IPG <128271467+eniac18000@users.noreply.github.com>
2023-11-10 18:53:49 +0330

    edite2023
    we

---
+++ web/assets/js/langs.js
@@ -0,0 +1,54 @@
+const supportLangs = [
+    {
+        name: 'English',
+        value: 'en-US',
+        icon: '🇺🇸',
+    },
+    {
+        name: 'فارسی',
+        value: 'fa-IR',
+        icon: '🇮🇷',
+    },
+   
+];
+
+function getLang() {
+    let lang = getCookie('lang');
+
+    if (!lang) {
+        if (window.navigator) {
+            lang = window.navigator.language || window.navigator.userLanguage;
+
+            if (isSupportLang(lang)) {
+                setCookie('lang', lang, 150);
+            } else {
+                setCookie('lang', 'en-US', 150);
+                window.location.reload();
+            }
+        } else {
+            setCookie('lang', 'en-US', 150);
+            window.location.reload();
+        }
+    }
+
+    return lang;
+}
+
+function setLang(lang) {
+    if (!isSupportLang(lang)) {
+        lang = 'en-US';
+    }
+
+    setCookie('lang', lang, 150);
+    window.location.reload();
+}
+
+function isSupportLang(lang) {
+    for (l of supportLangs) {
+        if (l.value === lang) {
+            return true;
+        }
+    }
+
+    return false;
+}
